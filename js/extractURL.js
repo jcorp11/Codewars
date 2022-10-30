723c0548e4967c6633b98007dd02e718da86ee9e
Joaquin Huerta <joaco.011@gmail.com>
2022-10-30 11:43:21 -0300

    url domain

---
+++ js/extractURL.js
@@ -0,0 +1,15 @@
+function domainName(url){
+    let regex = /(https?:\/\/)?(www\.)?/
+    url = url.replace(regex, '');
+    // regex = /www\./
+    // url = url.replace(regex, '');
+    
+    regex = /\..*/g
+    url = url.replace(regex, '');
+    return url
+}
+
+let url = "https://www.cnet.com";
+console.log(domainName(url));
+url = "http://github.com/carbonfive/raygun"
+console.log(domainName(url));
